16fa4d3d3e9d8c3c76902193044247f88c4f4f89
Gabriel Grabka <ggrabka@hotmail.com>
2025-04-02 23:10:55 +0200

    Added Release Year and Rating to Front End

--- src/main/java/at/ac/fhcampuswien/fhmdb/ui/MovieCell.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/ui/MovieCell.java
@@ -14,7 +14,9 @@ public class MovieCell extends ListCell<Movie> {
     private final Label title = new Label();
     private final Label detail = new Label();
     private final Label genre = new Label();
-    private final VBox layout = new VBox(title, detail, genre);
+    private final Label releaseYear = new Label();
+    private final Label rating = new Label();
+    private final VBox layout = new VBox(title, detail, genre, releaseYear, rating);
 
     @Override
     protected void updateItem(Movie movie, boolean empty) {
@@ -37,6 +39,8 @@ public class MovieCell extends ListCell<Movie> {
                     .map(Enum::toString)
                     .collect(Collectors.joining(", "));
             genre.setText(genres);
+            releaseYear.setText("Release Year: " + String.valueOf(movie.getReleaseYear()));
+            rating.setText("Rating: " + String.valueOf(movie.getRating()));
 
 
             // color scheme
@@ -44,6 +48,8 @@ public class MovieCell extends ListCell<Movie> {
             detail.getStyleClass().add("text-white");
             genre.getStyleClass().add("text-white");
             genre.setStyle("-fx-font-style: italic");
+            releaseYear.getStyleClass().add("text-white");
+            rating.getStyleClass().add("text-white");
             layout.setBackground(new Background(new BackgroundFill(Color.web("#454545"), null, null)));
 
             // layout
