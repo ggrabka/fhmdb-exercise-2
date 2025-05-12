e816ecac5a5897568c850cac4c72f8640ecb467f
stefan schoen <stefan.schoen@stud.fh-campuswien.ac.at>
2025-05-12 17:49:50 +0200

    fertig1

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,14 +6,22 @@ module at.ac.fhcampuswien.fhmdb {
     requires com.google.gson;
     requires java.net.http;
     requires org.apache.httpcomponents.httpclient;
+    requires org.apache.httpcomponents.httpcore;
     requires java.sql;
     requires ormlite.core;
     requires ormlite.jdbc;
 
     opens at.ac.fhcampuswien.fhmdb to javafx.fxml;
     opens at.ac.fhcampuswien.fhmdb.models to com.google.gson;
-    exports at.ac.fhcampuswien.fhmdb.models;
     exports at.ac.fhcampuswien.fhmdb;
+    exports at.ac.fhcampuswien.fhmdb.database; // <--- WICHTIG für ORMLite
+    exports at.ac.fhcampuswien.fhmdb.models;
+    exports at.ac.fhcampuswien.fhmdb.ui;
+    exports at.ac.fhcampuswien.fhmdb.util;
+    exports at.ac.fhcampuswien.fhmdb.logic;
+
+
+    opens at.ac.fhcampuswien.fhmdb.database to ormlite.core; // <--- Fügt Zugriffsrecht für Reflektion hinzu
 
 
 }
